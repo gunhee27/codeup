cdfb4bc0bc595c788e4a5b6338a78c37181149f2
geonhee27 <>
2022-01-31 12:58:53 +0000

    220131

---
+++ algorithm/6009-6024.py
@@ -0,0 +1,77 @@
+#6009
+a = input()
+print(a)
+
+#6010
+a = input()
+a = int(a)
+print(a)
+
+#6011
+a = input()
+a = float(a)
+print(a)
+
+#6012
+a = input()
+b = input()
+print(a)
+print(b)
+
+#6013
+a = input()
+b = input()
+print(b)
+print(a)
+
+#6014
+a = input()
+print(a)
+print(a)
+print(a)
+
+#6015
+a,b = input().split()
+print(a)
+print(b)
+
+#6016
+a,b = input().split()
+print(b)
+print(a)
+
+#6017
+a = input()
+print(a,a,a)
+
+#6018
+a,b = input().split(':')
+print(a,b,sep=':')
+
+#6019
+y,m,d = input().split('.')
+print(d,m,y,sep="-")
+
+#6020
+a,b = input().split('-')
+print(a,b,sep='')
+
+#6021
+a = input()
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+
+#6022
+a = input()
+print(a[0:2],a[2:4],a[4:6])
+
+#6023
+h,m,s = input().split(":")
+print(m)
+
+#6024
+a,b = input().split()
+print(a,b,sep='')
